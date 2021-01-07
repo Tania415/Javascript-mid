0cabb0352a3b9f8a9afef88c5c4f1fa0c404073a
Tania415 <72668076+Tania415@users.noreply.github.com>
2021-01-08 01:41:08 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,30 @@
+//Question no.01
+
+//function ourFuntionwithArgs(a,b) {
+  //  console.log(a,-b);
+//}
+
+//ourFuntionwithArgs(10,-5);
+
+//Question no.2
+
+
+
+
+//Question no.5
+//let fName = ['tamanna'],['jothe'],['tonni'],['rupa'];
+
+//function fName(word) {
+    //if (word < tamanna) {
+        //return "Large"
+    //}else if (word < jothe){
+       // return "Medium"
+    //}else if (word < rupa){
+        //return "Small"
+    //}else{
+        //return "Tiny"
+   // }
+//}
+
+//fName(4);
+//console.log(fName(4));
